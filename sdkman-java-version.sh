30bfbca2b72e1a00c69e89c9f02a71b279ee3278
hbdesiato <hbdesiato@gmx.de>
2020-01-29 09:31:16 +0100

    hotfix missing openjdk 13

--- sdkman-java-version.sh
+++ sdkman-java-version.sh
@@ -2,7 +2,7 @@
 curl -s https://api.sdkman.io/2/candidates/java/Linux64/versions/list?installed= | {
     VERSION=0
     while read line ; do
-        if [[ $line =~ ([0-9]*)([^ ]*)-open ]] ; then
+        if [[ $line =~ ([0-9]*)([^ ]*)-zulu ]] ; then
             if [[ ${BASH_REMATCH[2]} != .ea* && $VERSION -lt ${BASH_REMATCH[1]} ]] ; then
                 VERSION=${BASH_REMATCH[1]}
                 RESULT=${BASH_REMATCH[0]}
